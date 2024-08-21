ca7385fca17a5d1248c45569b9449974bf6d1ca7
quentinmathieu <quentin.mth@gmail.com>
2024-08-21 16:24:16 +0200

    api test

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,19 +1,53 @@
-import { Link } from 'expo-router';
-import { View, Text, StyleSheet } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, FlatList, Text, View} from 'react-native';
+import axios from 'axios';
 
-export default function HomeScreen() {
+const baseUrl = 'https://sf-ecommerce.maqu6194.odns.fr/api/products';
+
+type Product = {
+  id: string;
+  name: string;
+};
+
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<Product[]>([]);
+
+  const getProducts = async () => {
+    try {
+      const response = await axios.get(
+          `https://sf-ecommerce.maqu6194.odns.fr/api/products`
+        );
+      console.log(response.data["hydra:member"]);
+      // const json = await response.json();
+      setData(response.data["hydra:member"]);
+
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Home</Text>
-      <Link href="/details">View details</Link>
+    <View style={{flex: 1, padding: 24}}>
+      {(
+        <FlatList
+          data={data}
+          keyExtractor={({id}) => id}
+          renderItem={({item}) => (
+            <Text>
+              {item.name}
+            </Text>
+          )}
+        />
+      )}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+export default App;
